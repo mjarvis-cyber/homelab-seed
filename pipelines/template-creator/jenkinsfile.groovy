22102f96677f31b328ec28d8d9bacbb39dd3ef69
Jarvis <mikeyjar1@gmail.com>
2024-06-21 20:07:20 -0400

    restructure seed job

--- pipelines/template-creator/jenkinsfile.groovy
+++ pipelines/template-creator/jenkinsfile.groovy
@@ -1,51 +1,28 @@
-pipelineJob('TestPipeline') {
-    displayName('Test pipeline for DSL')
-    description('Test pipeline for DSL')
-
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/OrangeSquirter/homelab-seed.git')
-                    }
-                    branch('master')
-                }
-            }
-            scriptPath('pipelines/template-creator/jenkinsfile.groovy')
+pipeline {
+    agent {
+        dockerfile {
+            filename 'Dockerfile'
         }
     }
-    definition {
-        cps {
-            script("""
-                pipeline {
-                    agent {
-                        dockerfile {
-                            filename 'Dockerfile'
-                        }
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout([$class: 'GitSCM',
+                    branches: [[name: '*/master']],
+                    doGenerateSubmoduleConfigurations: false,
+                    extensions: [],
+                    userRemoteConfigs: [[url: 'https://github.com/OrangeSquirter/homelab-seed.git']]
+                ])
+            }
+        }
+        stage('Build Templates') {
+            steps {
+                script {
+                    dir('pipelines/template-creator') {
+                        sh 'python template-creator.py'
                     }
-                    stages {
-                        stage('Checkout') {
-                            steps {
-                                checkout([$class: 'GitSCM',
-                                    branches: [[name: '*/master']],
-                                    doGenerateSubmoduleConfigurations: false,
-                                    extensions: [],
-                                    userRemoteConfigs: [[url: 'https://github.com/OrangeSquirter/homelab-seed.git']]
-                                ])
-                            }
-                        }
-                        stage('Build Templates') {
-                            steps {
-                                script {
-                                    dir('pipelines/template-creator') {
-                                        sh 'python template-creator.py'
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }""")
+                }
+            }
         }
     }
 }
--- pipelines/test/jenkinsfile.groovy
+++ pipelines/test/jenkinsfile.groovy
@@ -1,47 +1,13 @@
-pipelineJob('TestPipeline') {
-    displayName('Test pipeline for DSL')
-    description('Test pipeline for DSL')
-
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/OrangeSquirter/homelab-seed.git')
-                    }
-                    branch('master')
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+             steps {
+                script {
+                    cleanWs()
+                        sh 'echo hello world!'
                 }
             }
-            scriptPath('pipelines/test/jenkinsfile.groovy')
-        }
-    }
-
-    triggers {
-        cron('H * * * *')
-    }
-
-    parameters {
-        stringParam('PARAM_NAME', 'default_value', 'Description of parameter')
-    }
-
-    definition {
-        cps {
-            script("""
-                pipeline {
-                    agent any
-                    stages {
-                        stage('Build') {
-                            steps {
-                                script {
-                                    cleanWs()
-                                    sh 'echo hello world!'
-                                }
-                            }
-                        }
-                        // Add more stages as needed
-                    }
-                }
-            """)
         }
     }
 }
