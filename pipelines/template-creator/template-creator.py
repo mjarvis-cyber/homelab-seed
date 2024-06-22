d5f312b24dc50408f0bfc7697cc4b1f05f446eb0
Jarvis <mikeyjar1@gmail.com>
2024-06-22 11:52:28 -0400

    Start fresh on template  creator

--- pipelines/template-creator/template-creator.py
+++ pipelines/template-creator/template-creator.py
@@ -1,60 +1,54 @@
 import requests
 import json
-import subprocess
-import os
+import argparse
 
-def get_current_vmids():
-    vm_metadata = "/etc/pve/.vmlist"
-    with open(vm_metadata, 'r') as file:
-        data = json.load(file)
-    return list(data['ids'].keys())
+def get_cluster_query_output(cluster_query, proxmox_ip, token_name, token_secret):
+    api_url = f"https://{proxmox_ip}:8006/{cluster_query}"
+    headers = {
+        "Authorization": f"PVEAPIToken={token_name}={token_secret}"
+    }
+    
+    response = requests.get(api_url, headers=headers, verify=False)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
 
-def pick_vmid(target_id, end_id):
-    current_ids = get_current_vmids()
-    while target_id in current_ids:
-        if target_id == end_id:
-            raise Exception("No VM IDs available.")
-        target_id += 1
-    return target_id
+def get_vm_metadata(proxmox_ip, token_name, token_secret):
+    vm_data = {}
+    vmids = get_cluster_query_output("api2/json/cluster/resources", proxmox_ip, token_name, token_secret)["data"]
+    for vm in vmids:
+        if vm["type"] == "qemu":
+            vm_data[vm["vmid"]] = {}
+            vm_data[vm["vmid"]]["proxmox_node"] = vm["node"]
+            vm_data[vm["vmid"]]["status"] = vm["status"]
+    return vm_data
 
-def get_qcow(image_url, qcow_dir, qcow_file, name):
-    os.makedirs(qcow_dir, exist_ok=True)
-    qcow_path = f"/tmp/{name}.img"
-    subprocess.run(["wget", "-O", qcow_path, image_url])
-    subprocess.run(["mv", qcow_path, qcow_file])
-    subprocess.run(["qemu-img", "resize", qcow_file, "32G"])
+def pick_vmid(proxmox_ip, token_name, token_secret, vmid_start, vmid_end):
+    vm_metadata = get_vm_metadata(proxmox_ip, token_name, token_secret)
+    used_vmids = vm_metadata.keys()
+    for vmid in range(vmid_start, vmid_end + 1):
+        if vmid not in used_vmids:
+            return vmid
+    raise ValueError("No available VMID found in the specified range")
 
-def disk_config(vmid, qcow_location):
-    subprocess.run(["qm", "importdisk", str(vmid), qcow_location, "local-lvm"])
-    subprocess.run(["qm", "set", str(vmid), "-scsihw", "virtio-scsi-pci", "-virtio0", f"local-lvm:vm-{vmid}-disk-0"])
-    subprocess.run(["qm", "set", str(vmid), "-serial0", "socket"])
-    subprocess.run(["qm", "set", str(vmid), "-boot", "c", "-bootdisk", "virtio0"])
-
-def misc_config(vmid):
-    subprocess.run(["qm", "set", str(vmid), "-hotplug", "disk,network,usb"])
-    subprocess.run(["qm", "set", str(vmid), "-vga", "qxl"])
-    subprocess.run(["qm", "set", str(vmid), "--onboot", "1"])
-
-def create_vm(vmid, name):
-    subprocess.run(["qm", "create", str(vmid), "-name", name, "-memory", "1024", "-net0", "virtio,bridge=vmbr0", "-cores", "1", "-sockets", "1"])
-
-def template_resource_pool(vmid, resource_pool):
-    subprocess.run(["pvesh", "create", "/pools", "--poolid", resource_pool])
-
-def vm_creation_pipeline(vmid, name, image_url, ssh_keys, qcow_dir, user, password, ip_to_use):
-    qcow_file = f"{qcow_dir}/{name}.qcow2"
-    get_qcow(image_url, qcow_dir, qcow_file, name)
-    create_vm(vmid, name)
-    disk_config(vmid, qcow_file)
-    misc_config(vmid)
-    subprocess.run(["qm", "template", str(vmid)])
-
-def runner(resource_pool_name, name, vmid_start, vmid_end, qcow_dir, ssh_keys, image_location, user, password, ip_to_use):
-    vmid = pick_vmid(vmid_start, vmid_end)
-    vm_creation_pipeline(vmid, name, image_location, ssh_keys, qcow_dir, user, password, ip_to_use)
-    template_resource_pool(vmid, resource_pool_name)
+def runner(proxmox_ip, token_name, token_secret, resource_pool_name, name, vmid_start, vmid_end, qcow_dir, ssh_keys, image_location, user, password, ip_to_use):
+    vmid = pick_vmid(proxmox_ip, token_name, token_secret, vmid_start, vmid_end)
+    print(f"Here is the vmid to use: {vmid}")
 
 def main():
+    parser = argparse.ArgumentParser(description="Create Proxmox templates")
+    parser.add_argument("--proxmox_ip", required=True, help="Proxmox IP address")
+    parser.add_argument("--token_name", required=True, help="Proxmox API token name")
+    parser.add_argument("--token_secret", required=True, help="Proxmox API token secret")
+
+    args = parser.parse_args()
+
+    proxmox_ip = args.proxmox_ip
+    token_name = args.token_name
+    token_secret = args.token_secret
+
     with open("configs.json", "r") as file:
         config = json.load(file)
 
@@ -64,6 +58,9 @@ def main():
             file.write("\n".join(template['ssh_keys']))
 
         runner(
+            proxmox_ip,
+            token_name,
+            token_secret,
             config['resource_pool'],
             template_name,
             config['template_start_id'],
