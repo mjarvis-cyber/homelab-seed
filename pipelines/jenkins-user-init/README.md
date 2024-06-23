4a4e59edb325e597e39fcbf8a1b63d18f6695cec
Jarvis <mikeyjar1@gmail.com>
2024-06-23 13:22:14 -0400

    Build jenkins api token pipeline

---
+++ pipelines/jenkins-user-init/README.md
@@ -0,0 +1,3 @@
+# Jenkins User Init
+
+This pipeline simply creates an API key for the jenkins user and saves it. Only run once
---
+++ pipelines/jenkins-user-init/dsl.groovy
@@ -0,0 +1,19 @@
+pipelineJob('jenkins-user-init') {
+    displayName('Jenkins User Init')
+    description('Initialize jenkins user, only run once.')
+
+    definition {
+        cpsScm {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/OrangeSquirter/homelab-seed.git')
+                    }
+                    branch('master')
+                }
+            }
+            scriptPath('pipelines/jenkins-user-init/Jenkinsfile')
+        }
+    }
+}
