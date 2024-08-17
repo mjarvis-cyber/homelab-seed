d61f49faa3c12daa4bd72dded4a9f4e1a46a1c12
Jarvis <mikeyjar1@gmail.com>
2024-08-16 21:39:52 -0400

    make jenkins ssh to itself

--- pipelines/jenkins-agent-builder/deploy-agent.py
+++ pipelines/jenkins-agent-builder/deploy-agent.py
@@ -1,13 +1,19 @@
 import argparse
 import json
 import paramiko
-import subprocess
 import re
 from ipaddress import ip_network, ip_address
 
-def get_network_info():
-    result = subprocess.run(['ip', 'addr', 'show'], capture_output=True, text=True)
-    network_info = result.stdout
+def get_network_info(master_ip, ssh_key):
+    key = paramiko.RSAKey(file_obj=ssh_key)
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(master_ip, username='ubuntu', pkey=key)
+    
+    stdin, stdout, stderr = ssh.exec_command('ip addr show')
+    network_info = stdout.read().decode()
+    ssh.close()
+    
     ip_addresses = []
     for line in network_info.splitlines():
         if 'inet ' in line:
@@ -55,6 +61,7 @@ def main():
     parser.add_argument('--ssh-key-file', required=True, help='Path to the SSH private key file.')
     parser.add_argument('--scp-dir', required=True, help='Path to the directory to SCP.')
     parser.add_argument('--agent-name', required=True, help='Name of the Jenkins agent.')
+    parser.add_argument('--master-ip', required=True, help='IP address of the Jenkins master.')
 
     args = parser.parse_args()
     with open(args.metadata_file) as f:
@@ -64,7 +71,7 @@ def main():
 
     with open(args.secret_file) as f:
         secret = f.read().strip()
-    network_info = get_network_info()
+    network_info = get_network_info(args.master_ip, open(args.ssh_key_file, 'r'))
     master_ip = find_matching_ip(vm_ipv4, network_info)
     if not master_ip:
         print("No matching IP found in the same subnet.")
