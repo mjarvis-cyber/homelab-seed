04671778e2cef44338596d5b468bae50c15b1849
Jarvis <mikeyjar1@gmail.com>
2024-10-13 15:07:40 -0400

    Fix bake

--- pipelines/docker-bake/build.sh
+++ pipelines/docker-bake/build.sh
@@ -8,8 +8,8 @@ USERNAME=$4
 PASSWORD=$5
 
 # Ensure all required parameters are provided
-if [ -z "$REGISTRY" ] || [ -z "$DOCKER_BAKE_FILE" ] || [ -z "$REPO_URL" ] || [ -z "$IMAGE_NAME" ] || [ -z "$IMAGE_TAG" ] || [ -z "$USERNAME" ] || [ -z "$PASSWORD" ]; then
-  echo "Missing required arguments. Usage: ./build.sh <REGISTRY> <DOCKER_BAKE_FILE> <REPO_URL> <IMAGE_NAME> <IMAGE_TAG> <USERNAME> <PASSWORD>"
+if [ -z "$REGISTRY" ] || [ -z "$REPO_URL" ] || [ -z "$DOCKER_BAKE_FILE" ] || [ -z "$USERNAME" ] || [ -z "$PASSWORD" ]; then
+  echo "Missing required arguments. Usage: ./build.sh <REGISTRY> <REPO_URL> <DOCKER_BAKE_FILE> <IMAGE_NAME> <IMAGE_TAG> <USERNAME> <PASSWORD>"
   exit 1
 fi
 
@@ -17,7 +17,7 @@ echo "Cloning the repository from ${REPO_URL}..."
 git clone "${REPO_URL}" repo && cd repo || exit 1
 
 # Login to the Docker registry
-echo "${PASSWORD}" | docker login "${REGISTRY}" --username "${USERNAME}" --password-stdin
+echo "${PASSWORD}" | docker login "https://${REGISTRY}" --username "${USERNAME}" --password-stdin
 
 # Create the buildx builder (if not already created)
 docker buildx create --use || true
@@ -27,3 +27,5 @@ docker buildx bake -f "${DOCKER_BAKE_FILE}" --push
 
 # Print success message
 echo "Docker image ${IMAGE_NAME}:${IMAGE_TAG} successfully built and pushed to ${REGISTRY}"
+
+cd ../ && rm -rf repo
